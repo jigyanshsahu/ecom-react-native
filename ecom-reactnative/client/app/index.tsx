8f3891569f801f2953768192819f67849056672f
Jigyansh sahu <indianinja008@gmail.com>
2026-04-04 08:30:37 +0530

    tailwind done

--- ecom-reactnative/client/app/index.tsx
+++ ecom-reactnative/client/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-3xl font-bold  text-red-500">Hello NativeWind!</Text>
     </View>
   );
 }
