c276afc5cb7461cea23a54962ec946550fcd87dd
Jigyansh sahu <indianinja008@gmail.com>
2026-04-09 18:18:12 +0530

    add gesture control

--- ecom-reactnative/client/app/_layout.tsx
+++ ecom-reactnative/client/app/_layout.tsx
@@ -2,13 +2,15 @@ import { Stack } from 'expo-router';
 import "../global.css";
 import { CartProvider } from '@/context/CartContext';
 import { WishListProvider } from '@/context/WishListcontext';
-
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 export default function RootLayout() {
   return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
     <CartProvider>
       <WishListProvider>
         <Stack screenOptions={{ headerShown: false }} />
       </WishListProvider>
     </CartProvider>
+    </GestureHandlerRootView>
   );
 }
--- ecom-reactnative/client/components/ProductCard.tsx
+++ ecom-reactnative/client/components/ProductCard.tsx
@@ -4,9 +4,11 @@ import { ProductCardProps } from "@/constants/types";
 import { Link } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { COLORS } from "@/constants";
+import { useWishlist } from "@/context/WishListcontext";
 
 export default function ProductCard({ product }: ProductCardProps) {
-  const isLiked = false;
+  const {toggleWishlist,isInWishlist} = useWishlist();
+  const isLiked = isInWishlist(product._id);
 
   return (
     <Link href={`/product/${product._id}`} asChild>
@@ -23,8 +25,9 @@ export default function ProductCard({ product }: ProductCardProps) {
           {/* Favorite Icon */}
           <TouchableOpacity
             className="absolute top-2 right-2 z-10 p-2 bg-white rounded-full shadow-sm"
-            onPress={() => {
-              console.log("Liked");
+            onPress={(e) => {
+              e.stopPropagation(); 
+              toggleWishlist(product);
             }}
           >
             <Ionicons
